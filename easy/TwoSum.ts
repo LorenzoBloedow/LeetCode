f5fe184ef6ec75eeffa749d6bc263ab722d8ae5a
Lorenzo Bloedow <108244120+LorenzoBloedow@users.noreply.github.com>
2022-06-30 23:10:28 -0300

    Create TwoSum.ts

---
+++ easy/TwoSum.ts
@@ -0,0 +1,17 @@
+function twoSum(nums: number[], target: number): number[] {
+    let otherSum: number = 0;
+    for (let i: number = 0; i < nums.length; i++) {
+        otherSum = target - nums[i];
+        if (nums.indexOf(otherSum) !== -1) {
+            if (nums.indexOf(otherSum) === i) {
+                if (nums.indexOf(otherSum, (i + 1)) !== -1) {
+                    return [nums.indexOf(otherSum),
+                            nums.indexOf(otherSum, nums.indexOf(otherSum) + 1)];
+                }
+                continue;
+            return [i, nums.indexOf(otherSum)]; 
+            }
+        return [i, nums.indexOf(otherSum)];                                 
+        }
+    }
+};
